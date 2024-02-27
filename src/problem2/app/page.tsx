15529eaa38a17d7ea5a833338aeecbc3be587827
bryanleezh <leebryan307@gmail.com>
2024-02-27 16:12:14 +0100

    Feat: Add navbar to main page

---
+++ src/problem2/app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import NavBar from "@/components/NavBar";
+
+export default function Home() {
+  return (
+      <div className="flex flex-col pt-5">
+        <NavBar />
+      </div>
+  );
+}
