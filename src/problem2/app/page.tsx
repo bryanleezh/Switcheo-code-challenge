fc3b086760fb557bfb8fdbfbf5133a256f5ca761
bryanleezh <leebryan307@gmail.com>
2024-02-29 12:37:36 +0200

    Feat: Add themeswitcher and mainform

--- src/problem2/app/page.tsx
+++ src/problem2/app/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
 import NavBar from "@/components/NavBar";
+import ThemeSwitch from "@/components/ThemeSwitch";
+import MainForm from "@/components/MainForm";
 
 export default function Home() {
   return (
-      <div className="flex flex-col pt-5">
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-row items-center justify-between pt-5 pr-5">
         <NavBar />
+        <ThemeSwitch />
       </div>
+      <div className="flex flex-grow items-center justify-center">
+        <MainForm />
+      </div>
+    </div>
   );
 }
