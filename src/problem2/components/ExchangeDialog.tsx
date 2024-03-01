67955ea409b06da5806c37df25e1f31bf39ad683
bryanleezh <leebryan307@gmail.com>
2024-03-02 01:14:37 +0200

    Feat: Add confirmation button

---
+++ src/problem2/components/ExchangeDialog.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { ExchangeDialogProps } from "@/lib/types";
+
+export function ExchangeDialog( { onHandleExchange } : ExchangeDialogProps) {
+
+    const resetCurrency = () => {
+        onHandleExchange();
+    }
+
+    return (
+        <AlertDialog>
+        <AlertDialogTrigger asChild>
+            <Button className="bg-border">Exchange</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>You have successfully  exchanged your tokens!</AlertDialogTitle>
+                <AlertDialogDescription>
+                    Great job, getting rich on cryptocurrency is not easy...all the best!
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogAction className="bg-border" onClick={resetCurrency}>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+        </AlertDialog>
+  )
+}
