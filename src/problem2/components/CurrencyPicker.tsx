921d494b73e7980c9d57949db32996a2aa03faaf
bryanleezh <leebryan307@gmail.com>
2024-03-02 01:14:29 +0200

    Feat: Add feature to pick currency

--- src/problem2/components/CurrencyPicker.tsx
+++ src/problem2/components/CurrencyPicker.tsx
@@ -12,57 +12,62 @@ import {
 } from "./ui/command";
 import {
   Popover,
-  PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { currencies } from "@/lib/data";
 import { ChevronDown } from "lucide-react";
+import { CurrencyPickerProps } from "@/lib/types";
+import Image from "next/image";
 
-export default function CurrencyPicker() {
-const [open, setOpen] = useState<boolean>(false);
-const [value, setValue] = useState<string>("");
- 
-  const selectedCurrency = currencies.find(currency => currency.currency === value);
+export default function CurrencyPicker( { initialValue, onInitialValueChange }: CurrencyPickerProps ) {
+    const [open, setOpen] = useState<boolean>(false);
+    const [value, setValue] = useState<string>(initialValue || "");
 
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[200px] justify-between"
-        >
-          {value
-            ? currencies.find((currency) => currency.currency === value)?.currency
-            : "Select framework..."}
-          <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent>
-        <CommandDialog open={open} onOpenChange={setOpen}>
-          <CommandInput placeholder="Type a currency or search..." />
-          <CommandList>
-            <CommandGroup>
-              {currencies.map(currency => (
-                <CommandItem
-                  key={currency.currency}
-                  value={currency.currency}
-                  onSelect={currentValue => {
-                    setValue(currentValue === value ? '' : currentValue);
-                    setOpen(false);
-                  }}
+    const currencyNames = currencies.map(currency => currency.currency);
+
+    useEffect(() => {
+        if (initialValue !== undefined && onInitialValueChange) {
+            onInitialValueChange(value);
+        }
+    }, [value])
+
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button
+                    variant="ghost"
+                    role="combobox"
+                    aria-expanded={open}
+                    className=" p-5"
                 >
-                  {currency.currency}
-                </CommandItem>
-              ))}
-              {currencies.length === 0 && <CommandEmpty>No currencies found.</CommandEmpty>}
-            </CommandGroup>
-          </CommandList>
-        </CommandDialog>
-      </PopoverContent>
-    </Popover>
-  );
+                    <Image src={`${value}.svg`} alt={value} width={30} height={30}/>
+                    <h2 className="pl-3">{value}</h2>
+                    <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <CommandDialog open={open} onOpenChange={setOpen}>
+                <CommandInput placeholder="Type a currency or search..." />
+                <CommandList>
+                    <CommandGroup>
+                    {currencyNames.map(currency => (
+                        <CommandItem
+                            key={currency}
+                            value={currency}
+                            onSelect={() => {
+                                setValue(currency);
+                                setOpen(false);
+                            }}
+                        >
+                            <Image src={`${currency}.svg`} alt={currency} width={30} height={30}/>
+                            <h2 className="pl-3">{currency}</h2>
+                        </CommandItem>
+                    ))}
+                    {currencies.length === 0 && <CommandEmpty>No currencies found.</CommandEmpty>}
+                    </CommandGroup>
+                </CommandList>
+            </CommandDialog>
+        </Popover>
+    );
 }
