e2e4f45d981829a2c60a466a1ca433d65363011a
bryanleezh <leebryan307@gmail.com>
2024-03-01 04:06:50 +0200

    Feat: Remove previous form info

--- src/problem2/components/MainForm.tsx
+++ src/problem2/components/MainForm.tsx
@@ -4,56 +4,44 @@ import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+import CurrencyPicker from "./CurrencyPicker";
 
 export default function MainForm() {
     return  (
-        <div>
-            <Card className="w-[350px]">
+        <div className="w-1/2 mx-auto">
+            <Card>
                 <CardHeader>
-                    <CardTitle>Create project</CardTitle>
-                    <CardDescription>Deploy your new project in one-click.</CardDescription>
+                    <CardTitle>Swap</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <form>
-                    <div className="grid w-full items-center gap-4">
-                        <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">Name</Label>
-                        <Input id="name" placeholder="Name of your project" />
+                        <div className="grid w-full items-center gap-4">
+                            <div id="hi" className="flex flex-col space-y-1.5 border rounded-md p-4">
+                                <Label htmlFor="name">You pay</Label>
+                                <div className="flex w-full max-w items-center space-x-2">
+                                    <Input id="paycurrency" type="number" placeholder="0" className="border-none"/>
+                                    <CurrencyPicker />
+                                </div>
+                            </div>
+                            <div className="flex flex-col space-y-1.5 border rounded-md p-4">
+                                <Label htmlFor="name">You receive</Label>
+                                <div className="flex w-full max-w items-center space-x-2">
+                                    <Input id="paycurrency" type="number" placeholder="0" className="border-none"/>
+                                    <CurrencyPicker />
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="framework">Framework</Label>
-                        <Select>
-                            <SelectTrigger id="framework">
-                            <SelectValue placeholder="Select" />
-                            </SelectTrigger>
-                            <SelectContent position="popper">
-                            <SelectItem value="next">Next.js</SelectItem>
-                            <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                            <SelectItem value="astro">Astro</SelectItem>
-                            <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                            </SelectContent>
-                        </Select>
-                        </div>
-                    </div>
                     </form>
                 </CardContent>
                 <CardFooter className="flex justify-between">
                     <Button variant="outline">Cancel</Button>
-                    <Button>Deploy</Button>
+                    <Button>Swap!</Button>
                 </CardFooter>
             </Card>
         </div>
