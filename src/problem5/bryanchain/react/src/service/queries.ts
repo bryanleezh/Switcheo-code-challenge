1df27df2998b819f07d7e5b1b4c295a6af43bbd8
bryanleezh <leebryan307@gmail.com>
2024-03-04 02:31:53 +0200

    Feat: Connect blockchain to client

--- src/problem5/bryanchain/react/src/service/queries.ts
+++ src/problem5/bryanchain/react/src/service/queries.ts
@@ -2,11 +2,25 @@ import axios from "axios";
 const url = "http://0.0.0.0:1317"
 
 export async function getAllResources() {
-    let api_url = ""
+    let api_url = url + "bryanchain/bryanchain/resource";
+    try {
+        const response = await axios.get(api_url);
+        console.log("Get all resources",response);
+        return response.data;
+    } catch (error) {
+        return error;
+    }
 }
 
-export async function getResourceById() {
-
+export async function getResourceById(id: number) {
+    let api_url = url + "bryanchain/bryanchain/resource/" + id;
+    try {
+        const response = await axios.get(api_url);
+        console.log("Get all resources",response);
+        return response.data;
+    } catch (error) {
+        return error;
+    }
 }
 
 export async function createResource() {
