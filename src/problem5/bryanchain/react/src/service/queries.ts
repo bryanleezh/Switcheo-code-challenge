861c4217b0597eeadfc3273e898103f92b4b5bab
bryanleezh <leebryan307@gmail.com>
2024-03-04 02:16:03 +0200

    Feat: Add new components for integrating CRUD into frontend

---
+++ src/problem5/bryanchain/react/src/service/queries.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+const url = "http://0.0.0.0:1317"
+
+export async function getAllResources() {
+    let api_url = ""
+}
+
+export async function getResourceById() {
+
+}
+
+export async function createResource() {
+
+}
+
+export async function updateResource() {
+
+}
+
+export async function deleteResource() {
+
+}
--- src/problem5/bryanchain/react/src/views/DataView.tsx
+++ src/problem5/bryanchain/react/src/views/DataView.tsx
@@ -4,7 +4,7 @@ export default function DataView() {
   return (
     <div>
       {/* Uncomment the following component to add a form for a `modelName` -*/}
-      {/* (<IgntCrud storeName="OrgRepoModule" itemName="modelName" />) */}
+      <IgntCrud storeName="OrgRepoModule" itemName="modelName" />     
     </div>
   );
 }
