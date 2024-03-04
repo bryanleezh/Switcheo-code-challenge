6a5249d6ff574a6c51eb2ad8492ff6c075b59734
bryanleezh <leebryan307@gmail.com>
2024-03-04 20:40:40 +0100

    Feat: Add axios req for remaining CRUD

--- src/problem5/bryanchain/react/src/service/queries.ts
+++ src/problem5/bryanchain/react/src/service/queries.ts
@@ -1,7 +1,9 @@
-import axios from "axios";
-// import { Resource } from "bryanchain-client-ts/lib/bryanchain.bryanchain/module";
+import axios, { AxiosError } from "axios";
 import { Resource } from "../utils/types";
 const url = "http://0.0.0.0:1317/"
+
+// ! There might be a need for authorization for this to actually be secure, however, even without
+// ! authorization the request could go through but nothing can be stored on the blockchain
 
 export async function getAllResources(): Promise<Resource[]> {
     let api_url = url + "bryanchain/bryanchain/resource";
@@ -18,7 +20,7 @@ export async function getResourceById(id: number) : Promise<Resource>{
     let api_url = url + "bryanchain/bryanchain/resource/" + id;
     try {
         const response = await axios.get(api_url);
-        console.log("Get all resources",response);
+        console.log("Get resource",response);
         return response.data;
     }catch (error: any) {
         throw new Error("Error fetching resources: " + error.message);
@@ -35,17 +37,42 @@ export async function createResource(title: string, body: string) {
     }
     try {
         const response = await axios.post(api_url, req_body);
-        console.log("Get all resources",response);
+        console.log("Create resource",response);
         return response.data;
     }catch (error: any) {
         throw new Error("Error fetching resources: " + error.message);
     }
 }
 
-export async function updateResource() {
-
+export async function updateResource(id: string, title: string, body: string) {
+    const creator = "cosmos1ucjj85v02u4zj3ueny67xdw3glzxl5j0ygqrv8"
+    let api_url = url + "bryanchain/bryanchain/resource/" + id;
+    const req_body = {
+        id: id,
+        creator: creator,
+        body: body,
+        title: title
+    }
+    try {
+        const response = await axios.put(api_url);
+        console.log("Update resource: ",response);
+        return response.data;
+    }catch (error: any) {
+        throw new Error("Error fetching resources: " + error.message);
+    }
 }
 
-export async function deleteResource() {
-
+export async function deleteResource(id: string) {
+    const creator = "cosmos1ucjj85v02u4zj3ueny67xdw3glzxl5j0ygqrv8"
+    let api_url = url + "bryanchain/bryanchain/resource/" + id;
+    const req_body = {
+        id: id
+    }
+    try {
+        const response = await axios.delete(api_url);
+        console.log("Delete resource",response);
+        return response.data;
+    }catch (error: any) {
+        throw new Error("Error fetching resources: " + error.message);
+    }
 }
